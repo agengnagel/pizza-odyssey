a4cf7b819359e16ea5f57d218d86859b948db747
agengnagel <adriennegengnagel@hotmail.com>
2016-02-17 16:53:50 -0800

    got the table to work (thanks to the lecture), i think i need to make my hourlyPizza and hourlyDeliveries global variables to use when making my ballard variable out of my constrictor function

--- app.js
+++ app.js
@@ -2,53 +2,79 @@
 
 function Pizza3001(locationName, storeData) {
   this.locationName= locationName;
+  this.openhours=['8:00am', '9:00am','10:00am', '11:00am','12:00pm', '1:00pm','12:00pm', '1:00pm','2:00pm', '3:00pm','4:00pm', '5:00pm','6:00pm', '7:00pm','8:00pm', '9:00pm','10:00pm', '11:00pm','12:00am', '1:00am','2:00am'];
   this.storeData= storeData;
   this.hourlyPizzas = [];
   this.hourlyDeliveries= [];
-  this.makeRandoms();
-}
+};
 
-function generateRandom (min, max) {
-  return Math.floor((Math.random() * (max - min) + 1)) + min;
-}
+var ballardData = [[0,4,0,4],[0,7,0,4],[2,15,1,4], [15,35,3,8], [12,31,5,12] [5,20,6,11]];
+var firsthillData = [[1,3,1,7],[ 5,9,2,8],[2,13,1,6], [18,32,3,9], [1,3,5,12] [8,20,6,16]];
+var firsthill= new Pizza3001 ('First Hill', firsthillData);
+
+Pizza3001.prototype.generateRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 Pizza3001.prototype.makeRandoms= function (){
   for (var i=0; i<this.storeData.length; i++) {
     for (var j=0; j<3; j++) {
-      var tempPizza= generateRandom ([i][0], [i][1])
+      var tempPizza= this.generateRandom ([i][0], [i][1])
       this.hourlyPizzas.push(tempPizza);
-      var tempDeliver= generateRandom ([i][2],[i][3]);
+      var tempDeliver= this.generateRandom ([i][2],[i][3]);
       this.hourlyDeliveries.push(tempDeliver);
     }
   }
 };
 
-var ballardData = [[0,4,0,4],[0,7,0,4],[2,15,1,4], [15,35,3,8], [12,31,5,12] [5,20,6,11]];
+var ballard = new Pizza3001 ('Ballard', ballardData);
+ballard.makeRandoms();
 
-var firsthillData = [[1,3,1,7],[ 5,9,2,8],[2,13,1,6], [18,32,3,9], [1,3,5,12] [8,20,6,16]];
 
-var ballard = new Pizza3001 ('ballard', ballardData);
-var firsthill= new Pizza3001 ('firsthill', firsthillData);
+Pizza3001.prototype.render= function(){
+  this.makeRandoms();
+  var shopTable = document.createElement('pizzatable');
+  var trHeader1= document.createElement('tr');
 
-var pizzatable = document.getElementById('pizzatable');
-var tableHead = ['Hours'];
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.locationName;
+  trHeader1.appendChild(tdEl);
 
-var rowOne = createTableRow(ballard.hourlyPizzas);
-var rowTwo= createTableRow (ballard.hourlyDeliveries);
+  shopTable.appendChild(trHeader1);
 
-function createTableRow (infoArray) {
-  var newTR = document.createElement('tr');
-  for(var i=0; i<infoArray.length; i++){
-    var newTD = document.createElement('td');
-    newTD.textContent = infoArray[i];
-    newTR.appendChild(newTD);
+//second header
+  var trHeader2 = document.createElement('tr');
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Time';
+  trHeader2.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Pizzas';
+  trHeader2.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = 'Deliveries';
+  trHeader2.appendChild(tdEl);
+
+  shopTable.appendChild(trHeader2);
+
+  for (var i = 0; i < this.openhours.length; i++) {
+
+    var trEl = document.createElement('tr');
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.openhours[i];
+    trEl.appendChild(tdEl);
+
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.makeRandoms[i];
+    trEl.appendChild(tdEl);
+
+    shopTable.appendChild(trEl);
   }
-  return newTR;
+
+  document.body.appendChild(shopTable);
 }
-var headerRow = createTableRow(tableHead);
-var ballardRow = createTableRow(ballard.hourlyPizzas);
 
-
-pizzatable.appendChild(headerRow);
-pizzatable.appendChild(rowOne);
-pizzatable.appendChild(rowTwo);
+ballard.render();
+firsthill.render();
